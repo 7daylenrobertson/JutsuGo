b910411fa88a5d68405e542f2e7846904ba33938
7daylenrobertson <9daylenrobertson@gmail.com>
2020-08-15 05:06:30 -0400

    moves

--- main.py
+++ main.py
@@ -20,16 +20,23 @@ Builder.load_string("""
             background_color: 0,1,0,1
             text:"Click to start sensor"
             pos_hint:{"x":0.2,"y":0.3}
-            size_hint: 0.3 , 0.3
+            size_hint: 0.2 , 0.2
             on_release:
                 root.enable()
+        Button:
+            background_color: 0,1,0,1
+            text:"Click to submit"
+            pos_hint:{"x":0.2,"y":0.8}
+            size_hint: 0.2 , 0.2
+            on_release:
+                root.submit()
         
         Button:
             id:stop
             background_color: 1,0,0,1
             text:"Click to stop sensor"
             pos_hint:{"x":0.6,"y":0.3}
-            size_hint: 0.3 , 0.3
+            size_hint: 0.2 , 0.2
             on_release:
                 root.disable()
 
@@ -41,31 +48,53 @@ class MenuScreen(Screen):
 
     def enable(self):
         print("turned on")
-        counter=0
+        moves=[]
         accelerometer.enable()
         try:
             if accelerometer.acceleration[0]> 8.0:
                 tts.speak(message="Forward")
-                counter+=1
-                print(counter)
+                moves.append("f")
+                print(moves)
             elif accelerometer.acceleration[0]< -8.0:
                 tts.speak(message="Backward")
-                counter+=1
-                print(counter)
+                moves.append("b")
+                print(moves)
             elif accelerometer.acceleration[1]> 8.0:
                 tts.speak(message="Up")
+                moves.append("u")
+                print(moves)
             elif accelerometer.acceleration[1]< -8.0:
                 tts.speak(message="Down")
+                moves.append("d")
+                print(moves)
             elif accelerometer.acceleration[2]> 8.0:
-                tts.speak(message="Left")
-            elif accelerometer.acceleration[2]< -8.0:
                 tts.speak(message="Right")
+                moves.append("r")
+                print(moves)
+            elif accelerometer.acceleration[2]< -8.0:
+                tts.speak(message="Left")
+                moves.append("l")
+                print(moves)
         except:
             print("wait bruh")
 
     def disable(self):
-        accelerometer.disable()
-        print("turned OFF")   
+        moves=[]
+        tts.speak(message="Retarted!")
+        print(moves) 
+
+    def submit(self):
+        if moves==["f","b","f"]:
+            tts.speak(message="Fireball!")
+            moves=[]
+
+        elif moves==["l","u","r","u"]:
+            tts.speak(message="Lightning!") 
+            moves=[]
+
+        elif moves==["u","l","d","r","u"]:
+            tts.speak(message="Circle Of Pain!")
+            moves=[]  
             
     
 
